e0bee607b7358e58e65e6059a8574d38e3844467
phanisai166 <yogithasruthi@gmail.com>
2018-07-18 16:42:29 +0530

    project

--- fi.js
+++ fi.js
@@ -14,6 +14,7 @@ getfile("key.json",function(text){
   var data=JSON.parse(text);
   console.log(data);
   details(data.basics);
+  career(data.CareerObjective);
 }
 )
 var child=document.querySelector(".child1");
@@ -22,4 +23,35 @@ function details(det) {
   var img=document.createElement("img");
   img.src=det.image;
   child.appendChild(img);
+
+var name=document.createElement("name");
+name.textContent=det.name;
+child.appendChild(name);
+
+var phoneno=document.createElement("phoneno");
+phoneno.textContent=det.phoneno;
+child.appendChild(phoneno);
+
+var mail=document.createElement("a");
+mail.href = "mailto:yogithasruthi@gmail.com";
+mail.textContent=det.email;
+child.appendChild(mail);
+
+var add=document.createElement("add");
+add.textContent="Address";
+child.appendChild(add);
+
+var hr=document.createElement("hr");
+child.appendChild(hr);
+
+var address=document.createElement("a");
+address.textContent=det.Address;
+child.appendChild(address);
+
+}
+var child1=document.querySelector(".child2");
+function career(careerinfo){
+  var info=document.createElement("p");
+  info.textContent=careerinfo.info;
+  child1.appendChild(info);
 }
